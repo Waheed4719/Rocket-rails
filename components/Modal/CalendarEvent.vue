5b3ff3a5c5ce7b69d11170460154613089ed232e
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-08-24 23:04:41 +0600

    Fix minor issues in calendar event modal

--- components/Modal/CalendarEvent.vue
+++ components/Modal/CalendarEvent.vue
@@ -24,8 +24,8 @@
         <textarea
           rows="4"
           type="text"
-          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Event title"
+          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Event Description"
         />
       </div>
 
